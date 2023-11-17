ca3427e7297dac386dc09999c282b992bb44ae78
Kiokoshinkai <itoak7@gmail.com>
2023-11-17 16:58:04 +0300

    app fix8

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -53,7 +53,7 @@ const Register = ({ onRegister, errorMessage }) => {
                         label='E-mail'
                         name='email'
                         type='email'
-                        pattern='^([a-zA-Z0-9]([a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,6}$'
+                        pattern='^[-\w.]+@([A-z0-9][-A-z0-9]+\.)+[A-z]{2,4}$'
                         required
                         autoComplete='email'
                         value={values.email || ''}
