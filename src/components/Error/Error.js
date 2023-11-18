4193eddfcec713da80f8b53710d0795143a4a03d
Kiokoshinkai <itoak7@gmail.com>
2023-11-18 22:39:29 +0300

    app fix11

--- src/components/Error/Error.js
+++ src/components/Error/Error.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import './error.css'
 
 function NotFound() {
+  const navigate = useNavigate();
     return (
         <div className="error">
             <h1 className='error__title' >404</h1>
             <p className='error__subtitle' >Страница не найдена</p>
-            <Link to='/' className='error__link' rel='noreferrer'>Назад</Link>
+            <a href='/#' className='error__link' rel='noreferrer' onClick={(e) => {e.preventDefault(); navigate(-1)}}>Назад</a>
 
         </div>
     );
